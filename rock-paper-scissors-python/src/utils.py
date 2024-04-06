9fd44d7eb225d9a3fe6aa80d86b517fd6f19cc2e
Ayush Bhattarai <ayushbhattaraiofficial@gmail.com>
2024-04-06 09:47:04 +0545

    update(python version): created some constants
    - changed options and win_conditions from variables to constants (i.e. capitalized)

--- rock-paper-scissors-python/src/utils.py
+++ rock-paper-scissors-python/src/utils.py
@@ -3,24 +3,24 @@ from numpy.random import choice
 
 class Utility:
     def __init__(self):
-        self.win_conditions = {
+        self.WIN_CONDITIONS = {
             "ROCK": "PAPER",
             "PAPER": "SCISSORS",
             "SCISSORS": "ROCK",
         }
-        self.options = ["ROCK", "PAPER", "SCISSORS"]
+        self.OPTIONS = ["ROCK", "PAPER", "SCISSORS"]
 
     def getSystemChoice(self):
-        return choice(self.options)
+        return choice(self.OPTIONS)
 
     def getUserChoice(self):
         while True:
             self.user_choice = str(input("Enter rock, paper or scissor\n"))
-            if self.user_choice.upper() in self.options:
+            if self.user_choice.upper() in self.OPTIONS:
                 return self.user_choice.upper()
 
     def checkWinCondition(self, user_choice, system_choice):
-        return user_choice == self.win_conditions.get(system_choice)
+        return user_choice == self.WIN_CONDITIONS.get(system_choice)
 
     def checkLoseCondition(self, user_choice, system_choice):
-        return system_choice == self.win_conditions.get(user_choice)
+        return system_choice == self.WIN_CONDITIONS.get(user_choice)
